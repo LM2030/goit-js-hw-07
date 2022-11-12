c898cdbafe3a989995e38f6cdc42e27262dbec8d
LM2030 <108740598+LM2030@users.noreply.github.com>
2022-11-12 19:41:53 +0200

    Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryAll = document.querySelector(".gallery");
+const galleryCard = galleryItems.map(cardCreate).join("");
+
+galleryAll.insertAdjacentHTML("afterbegin", galleryCard);
+
+function cardCreate({ preview, original, description }) {
+	return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+}
+
+galleryAll.addEventListener("click", openModal);
+
+function openModal(event) {
+	event.preventDefault();
+
+	if (event.target.nodeName !== "IMG") {
+		return;
+	}
+	const modalAppearence = basicLightbox.create(`<div class="modal">
+  <img src="${event.target.dataset.source}" alt="${event.target.alt}" />
+  </div>`);
+	modalAppearence.show();
+
+	galleryAll.addEventListener("keydown", event => {
+		if (event.code === "Escape") {
+			modalAppearence.close();
+		}
+	});
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryAll = document.querySelector(".gallery");
+const galleryCard = galleryItems.map(cardCreate).join("");
+
+galleryAll.insertAdjacentHTML("afterbegin", galleryCard);
+
+function cardCreate({ preview, original, description }) {
+	return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
